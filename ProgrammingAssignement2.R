efbb527a7dc5098e5229ea3d9f23a8cff323653c
yasser <93216469+YasserBh21@users.noreply.github.com>
2021-11-13 23:51:40 +0100

    Update ProgrammingAssignement2.R

--- ProgrammingAssignement2.R
+++ ProgrammingAssignement2.R
@@ -1,20 +1,20 @@
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {  
   inv <- NULL
-  set <- function(y) {
+  set <- function(y) { #set value of the matrix
     x <<- y
     inv <<- NULL
   }
   get <- function() {x}
-  setinverse <- function(mean) inv <<- inv
+  setinverse <- function(mean) inv <<- inv  
   getinverse <- function() {inv}
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
-cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data")
+cacheSolve <- function(x, ...) { #compute the inverse oh a matrix 
+  inv <- x$getinverse()  #return inverse of matrix x
+  if(!is.null(inv)) { #retreave matrix from cashe if it is already existing
+    message("getting cached data") #if matrix is retreaved from cach the message chows up
     return(inv)
   }
   mat <- x$get()
